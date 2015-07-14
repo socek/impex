f231eb14613263abf477f4b5cfce6252e7b15bdb
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-07-14 20:33:59 +0200

    Permissions permissions...

--- src/impex/application/entryfactory.py
+++ src/impex/application/entryfactory.py
@@ -1,5 +1,6 @@
 from pyramid.security import Allow
 from pyramid.security import Authenticated
+from pyramid.security import Deny
 from pyramid.security import Everyone
 
 from .requestable import Requestable
@@ -8,9 +9,11 @@ from .requestable import Requestable
 class EntryFactory(Requestable):
     __acl__ = [
         (Allow, Everyone, 'view'),
-        (Allow, Authenticated, 'auth_view'),
+        (Deny, Authenticated, 'guest'),
+        (Allow, Everyone, 'guest'),
+        (Allow, Authenticated, 'auth'),
     ]
 
     def __init__(self, request):
         self.feed_request(request)
-        user = self.get_user()
+        self.user = self.get_user()
