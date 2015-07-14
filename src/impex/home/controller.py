abbc047b044ae16a89a69dc26759ddfe5306d109
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-07-14 22:32:35 +0200

    Cleaned up homepage.

--- src/impex/home/controller.py
+++ src/impex/home/controller.py
@@ -1,17 +1,7 @@
 from impex.application.controller import Controller
 
-from .forms import FirstForm
-
 
 class HomeController(Controller):
 
     renderer = 'impex.home:templates/me.haml'
     permission = 'auth'
-
-    def make(self):
-        data = self.drivers.SampleData.find_all()
-        self.context['data'] = data
-        form = self.add_form(FirstForm)
-        if form.validate():
-            self.add_flashmsg('info', 'success')
-            self.redirect('home')
