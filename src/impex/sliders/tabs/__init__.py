5893026049a84368923a5b813637ae67929ed28c
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2016-02-19 23:33:24 +0100

    Add logos.

--- src/impex/sliders/tabs/__init__.py
+++ src/impex/sliders/tabs/__init__.py
@@ -1,6 +1,6 @@
 from impex.application.requestable import Requestable
 from .scores import ScoresTabWidget
-from .slideshow import FirstTabWidget
+from .slideshow import LogaTabWidget
 # from .slideshow import SecondTabWidget
 
 
@@ -9,7 +9,7 @@ class TabList(Requestable):
     def __init__(self, request):
         self.feed_request(request)
         self.tabs = {}
-        self.add_tab(FirstTabWidget)
+        self.add_tab(LogaTabWidget)
         # self.add_tab(SecondTabWidget)
         self.add_tab(ScoresTabWidget)
         # self.add_tab(ChangeableTabWidget)
--- src/impex/sliders/tabs/slideshow.py
+++ src/impex/sliders/tabs/slideshow.py
@@ -2,8 +2,7 @@ from .base import TabWidget
 
 
 class SlideshowTabWidget(TabWidget):
-    host = 'http://localhost/images/'
-    local_path = None
+    host = 'http://turniejserv/'
     template = 'impex.sliders:templates/widgets/slideshow.haml'
 
     def _get_images(self):
@@ -15,18 +14,41 @@ class SlideshowTabWidget(TabWidget):
         self.context['images'] = self._get_images()
 
 
-class FirstTabWidget(SlideshowTabWidget):
+class LogaTabWidget(SlideshowTabWidget):
     name = 'first'
-    local_path = '/home/socek/zdjecia/mlk/do_gosi'
-    speed = 2
+    speed = 3
 
     images = [
-        '/do_gosi/IMG_0468.JPG',
-        '/do_gosi/IMG_0469.JPG',
-        '/do_gosi/IMG_0470.JPG',
-        '/do_gosi/IMG_0471.JPG',
-        '/do_gosi/IMG_0472.JPG',
-        '/do_gosi/IMG_0474.JPG',
+        'loga/azaria.jpg',
+        'loga/balnex.gif',
+        'loga/bi-med.png',
+        'loga/eltar.png',
+        'loga/fotografia-patryk.png',
+        'loga/gwarek.gif',
+        'loga/HI-TOM.png',
+        'loga/inivjatywa.gif',
+        'loga/integrac.gif',
+        'loga/iph.gif',
+        'loga/logo_bluvision.jpg',
+        'loga/logo_parkwodny.png',
+        'loga/logo-PWiK.jpg',
+        'loga/mbm.gif',
+        'loga/mosir.png',
+        'loga/mtbs.png',
+        'loga/noram.gif',
+        'loga/osa_trans.jpg',
+        'loga/oscomputerosft.png',
+        'loga/ralk_big.gif',
+        'loga/solver.png',
+        'loga/ssg.png',
+        'loga/tck.gif',
+        'loga/terrabud.png',
+        'loga/tgstacja.png',
+        'loga/turniej_Manualmed_logo.jpg',
+        'loga/tvs.jpg',
+        'loga/veolia.jpg',
+        'loga/winnice-eurazji.jpg',
+
     ]
 
 
@@ -34,3 +56,4 @@ class SecondTabWidget(SlideshowTabWidget):
     name = 'second'
     local_path = '/home/socek/zdjecia/mlk/second'
     speed = 2
+
